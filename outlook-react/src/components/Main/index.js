a6baa515949394508ca80a35bd1f9a6add763331
Maher Nahla <Maher.Nahla@wb.paltel.net>
2023-03-18 14:19:02 +0200

    adding images to public

---
+++ outlook-react/src/components/Main/index.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import FolderList from "./FolderList"
+import MailList from "./MailList"
+import MailBody from "./MailBody"
+
+const Main = () => {
+  return (
+    <>
+    <FolderList/>
+    <MailList/>
+    <MailBody />
+    </>
+  )
+}
+
+export default Main
